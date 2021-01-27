409157f9f51a1442d2fb32e42770aaca2c887a40
Piotr Gorski <gorskipiotr5@gmail.com>
2021-01-27 16:56:54 +0000

    poprawka

--- js/main.js
+++ js/main.js
@@ -3,14 +3,14 @@ let secondString = ("Jestem świetnym programistą");
 
 function longerString (string1, string2) {
     if (string1.length > string2.length) {
-        return ("Longer string is \"" + firstString + "\"");
+        return ("Longer string is \"" + string1 + "\"");
     } else if (string1.length < string2.length) {
-        return ("Longer string is \"" + secondString + "\"");
+        return ("Longer string is \"" + string2 + "\"");
     } else {
         return ("Strings are even");
     }
 }
 
-console.log("Lenght of string: \"" + `${firstString}` + "\" = " + `${firstString.length}`)
-console.log("Lenght of string: \"" + `${secondString}` + "\" = " + `${secondString.length}`)
+console.log("Lenght of string: \"" + firstString + "\" = " + firstString.length)
+console.log("Lenght of string: \"" + secondString + "\" = " + secondString.length)
 console.log(longerString(firstString, secondString));
